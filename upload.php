7dd3ef2a5eca6479b5d04f4c677d86af5927c40f
Ubuntu <ubuntu@ip-172-31-19-226.ec2.internal>
2021-07-06 17:07:09 +0000

    added UUID

--- upload.php
+++ upload.php
@@ -1,4 +1,29 @@
-<?php print_r($_FILES);
+<?php
+
+function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        // 32 bits for "time_low"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_mid"
+        mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand( 0, 0x0fff ) | 0x4000,
+
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand( 0, 0x3fff ) | 0x8000,
+
+        // 48 bits for "node"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+}
+
+
+ print_r($_FILES);
 $tmpfile = $_FILES['file'];
 print_r($tmpfile);
 
@@ -16,7 +41,8 @@ $s3 = new S3Client([
     'region'  => 'us-east-1'
 ]);
 
- $key = str_replace(".","-".rand(1,9999).".",$tmpfile['name']);
+$key= gen_uuid();
+
 
 try {
     $result = $s3->putObject([
@@ -25,9 +51,9 @@ try {
         'SourceFile' => $tmpfile['tmp_name'],
         'ACL'    => 'public-read',
     ]);
-    return $result['ObjectURL'];
     print_r($result['ObjectURL']);
 } catch (Aws\S3\Exception\S3Exception $e) {
     echo "There was an error uploading the file.\n";
 }
+
 ?>;
